f9918dd55cdcf19dd1a3f40454c34e66b4547ea2
generateui <rtimon@gmail.com>
2011-02-12 15:30:01 +0100

    Fixed more serialization warnings

--- src/java/soc/gwtClient/game/abstractWidgets/Server.java
+++
@@ -1,8 +0,0 @@
-package soc.gwtClient.game.abstractWidgets;
-
-import soc.common.actions.gameAction.AbstractGameAction;
-
-public interface Server
-{
-    public void sendAction(AbstractGameAction gameAction); 
-}
