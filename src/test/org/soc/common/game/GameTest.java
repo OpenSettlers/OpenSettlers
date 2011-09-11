cdc50bc96babd85a269071b9e5fb99a134756bf0
generateui <rtimon@gmail.com>
2011-09-11 22:05:59 +0200

    simplified Game test, added generic object unicity checker

--- src/test/org/soc/common/game/GameTest.java
+++ src/test/org/soc/common/game/GameTest.java
@@ -1,6 +1,7 @@
 package org.soc.common.game;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import org.junit.Test;
 import org.soc.common.game.GamePlayer.GamePlayerImpl;
@@ -10,23 +11,31 @@ import org.soc.common.game.board.StandardFourPlayer;
 
 public class GameTest {
   @Test public void testGame() {
-    Game game = new Game();
     Board board = new StandardFourPlayer();
-    game.setBoard(board);
+    Game game = new Game()
+            .setBoard(board);
     GamePlayer player0 = new GamePlayerImpl(0, "testPlayer0");
     GamePlayer player1 = new GamePlayerImpl(1, "testPlayer1");
     GamePlayer player2 = new GamePlayerImpl(2, "testPlayer2");
     GamePlayer player3 = new GamePlayerImpl(3, "testPlayer3");
-    assertTrue(player0.isNot(player1));
-    assertTrue(player0.isNot(player2));
-    assertTrue(player0.isNot(player3));
-    assertTrue(player1.isNot(player2));
-    assertTrue(player1.isNot(player3));
-    assertTrue(player2.isNot(player3));
+    assertThatAreUnique(player0, player1, player2, player3);
     game.players().add(player0, player1, player2, player3);
     assertTrue(game.players().size() == 4);
     HostStartsGame startGame = (HostStartsGame) new HostStartsGame()
             .setPlayer(player0);
     game.performAction(startGame);
+  }
+  private static void assertThatAreUnique(Object... objects) {
+    for (int i = 0; i < objects.length; i++) {
+      Object object = objects[i];
+      for (int j = 0; j < objects.length; j++) {
+        if (j != i) {
+          Object other = objects[j];
+          if (object.equals(other)) {
+            fail();
+          }
+        }
+      }
+    }
   }
 }
