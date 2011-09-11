b0b991478f87964f019929b7903d5bcf9a655201
generateui <rtimon@gmail.com>
2011-09-11 23:52:15 +0200

    More ResourceList tests

--- src/test/org/soc/common/game/ResourcesTest.java
+++ src/test/org/soc/common/game/ResourcesTest.java
@@ -8,9 +8,11 @@ import org.soc.common.game.Resource.Ore;
 import org.soc.common.game.Resource.Sheep;
 import org.soc.common.game.Resource.Timber;
 import org.soc.common.game.Resource.Wheat;
-import org.soc.common.game.ResourceList;
+import org.soc.common.game.ResourcesChangedEvent.ResourcesChangedHandler;
 
 public class ResourcesTest {
+  boolean hasFired;
+
   @Test public void testHalfCount() {
     ResourceList resources = new ResourceList();
     for (int i = 0; i < 9; i++)
@@ -18,16 +20,34 @@ public class ResourcesTest {
     assertTrue(resources.halfCount() == 5);
   }
   @Test public void testResourceTypesCount() {
-    ResourceList resources = new ResourceList();
-    resources.add(new Wheat());
-    resources.add(new Timber());
-    resources.add(new Ore());
-    resources.add(new Sheep());
-    resources.add(new Clay());
+    ResourceList resources = createFromFiveBasicResources();
     assertTrue(resources.ofType(new Wheat()).size() == 1);
     assertTrue(resources.ofType(new Timber()).size() == 1);
     assertTrue(resources.ofType(new Ore()).size() == 1);
     assertTrue(resources.ofType(new Sheep()).size() == 1);
     assertTrue(resources.ofType(new Clay()).size() == 1);
+  }
+  @Test public void testHasAtLeast() {
+    ResourceList resources = createFromFiveBasicResources();
+    assertTrue(resources.hasAtLeast(createFromFiveBasicResources()));
+  }
+  @Test public void testFiresEvents() {
+    ResourceList resources = new ResourceList();
+    resources.addResourcesChangedHandler(new ResourcesChangedHandler() {
+      @Override public void onResourcesChanged(ResourcesChangedEvent event) {
+        hasFired = true;
+        assertTrue(event.getAddedResources().get(0).equals(new Wheat()));
+      }
+    });
+    resources.add(new Wheat());
+    assertTrue(hasFired);
+  }
+  private static ResourceList createFromFiveBasicResources() {
+    return new ResourceList()
+            .add(new Wheat())
+            .add(new Timber())
+            .add(new Ore())
+            .add(new Sheep())
+            .add(new Clay());
   }
 }
